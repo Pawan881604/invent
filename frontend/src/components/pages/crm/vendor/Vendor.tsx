1099fe34cf7bf1acea837da8cce3e0843d21f9ab
gurez <boxbrothersocial@gmail.com>
2024-09-25 17:20:09 +0530

    asdfs

--- frontend/src/components/pages/crm/vendor/Vendor.tsx
+++ frontend/src/components/pages/crm/vendor/Vendor.tsx
@@ -5,12 +5,15 @@ import PageHeader from '@/components/common/Page_header'
 import Vendor_from from './Vendor_from'
 import Popover_component from '@/components/Popover_component/Popover_component'
 import { vendr_form } from '@/types/Vendor_type'
+import { useAddNew_vendorMutation } from '@/state/vendorApi'
+import { generate32BitUUID } from '@/lib/service/generate32BitUUID'
 
 const Vendor: React.FC = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
-
-    const onSubmit = (data: vendr_form) => {
-        console.log(data)
+    const [addNew_vendor, { data, error, isSuccess, isLoading }] = useAddNew_vendorMutation();
+    const onSubmit = async (data: vendr_form) => {
+        const updated_data = { ...data, uuid: generate32BitUUID() }
+        await addNew_vendor(updated_data)
     }
 
     return (
