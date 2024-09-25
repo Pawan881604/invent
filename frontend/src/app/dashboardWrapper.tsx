380438e8a7f0cd4831668474b97c676ea0f5427f
gurez <boxbrothersocial@gmail.com>
2024-09-25 09:10:30 +0530

    sdfsdf

--- frontend/src/app/dashboardWrapper.tsx
+++ frontend/src/app/dashboardWrapper.tsx
@@ -4,7 +4,12 @@ import React, { useEffect } from "react";
 import Navbar from "@/app/(components)/Navbar";
 import Sidebar from "@/app/(components)/Sidebar";
 
+import { useAppSelector } from "@/app/redux";
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
   // useEffect(() => {
   //   if (isDarkMode) {
   //     document.documentElement.classList.add("dark");
@@ -15,17 +20,15 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div
-      // className={`${
-      //   isDarkMode ? "dark" : "light"
-      // } flex bg-gray-50 text-gray-900 w-full min-h-screen dark:bg-[#1f1228]`}
-      className="flex bg-gray-50 text-gray-900 w-full min-h-screen dark:bg-[#1f1228]"
+      className={`${
+        isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen dark:bg-[#1f1228]`}
     >
       <Sidebar />
       <main
-        // className={`flex flex-col w-full h-full py-7 px-4 md:px-9 dark:bg-[#1f1228] ${
-        //   isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
-        // }`}
-        className="flex flex-col w-full h-full py-7 px-4 md:px-9 dark:bg-[#1f1228]"
+        className={`flex flex-col w-full h-full py-7 px-4 md:px-9 dark:bg-[#1f1228] ${
+          isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+        }`}
       >
         <Navbar />
         {children}
