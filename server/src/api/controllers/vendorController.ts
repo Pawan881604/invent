9c0e5bae24bc3252e88792d8e409514ed1a4fbe4
gurez <boxbrothersocial@gmail.com>
2024-09-26 09:29:26 +0530

    asdfs

--- server/src/api/controllers/vendorController.ts
+++ server/src/api/controllers/vendorController.ts
@@ -7,10 +7,25 @@ class VendorController {
   add_new = AsyncHandler.handle(
     async (req: Request, res: Response, next: NextFunction) => {
       const vendor = await this.vendorService.add_new_vendor(req.body, next);
-      res.status(201).json({
-        success: true,
-        vendor,
-      });
+
+      if (vendor) {
+        return res.status(201).json({
+          success: true,
+          vendor,
+        });
+      }
+    }
+  );
+  all_vendors = AsyncHandler.handle(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const vendor = await this.vendorService.all_vendors(req.body, next);
+
+      if (vendor) {
+        return res.status(201).json({
+          success: true,
+          vendor,
+        });
+      }
     }
   );
 }
