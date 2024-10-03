fed486c3d940bd99e900e612ffe16d28f201b319
gurez <boxbrothersocial@gmail.com>
2024-10-03 11:05:45 +0530

    sdfsdf

--- frontend/src/components/pages/crm/product-service/categories/Category.tsx
+++ frontend/src/components/pages/crm/product-service/categories/Category.tsx
@@ -1,14 +1,62 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Popover_component from "@/components/Popover_component/Popover_component";
 import Categotie_form from './Categotie_form';
 import { Button } from '@nextui-org/react';
+import { useAddNewCategorieMutation } from '@/state/categoriesApi';
+import { categorie_form } from '@/types/categorie_type';
+import toast from 'react-hot-toast';
 
 const Category = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const [files, setFiles] = useState<File[]>([]);
-    const onSubmit = () => { };
-    
+    const [addNewCategorie, { error, isLoading, isSuccess }] = useAddNewCategorieMutation();
+    const onSubmit = useCallback(
+        async (data: categorie_form) => {
+            await addNewCategorie(data)
+        },
+        [addNewCategorie],
+    )
+
+    useEffect(() => {
+        // Handle error messages
+        if (error) {
+            let errorMessage = "An unexpected error occurred."; // Default message
+
+            // Check if 'error' is defined and has the expected structure
+            if (error && 'data' in error) {
+                errorMessage = (error as { data?: { message?: string } }).data?.message || errorMessage;
+            }
+
+            //   // Check if 'update_error' is defined and has the expected structure
+            //   if (update_error && 'data' in update_error) {
+            //     errorMessage = (update_error as { data?: { message?: string } }).data?.message || errorMessage;
+            //   }
+
+            toast.error(errorMessage);
+            //   setOperationSuccess(false); // Reset success state on error
+            return; // Exit early if there's an error
+        }
+
+        // Handle success messages
+        if (isSuccess) {
+            //   setIsOpen(false);
+            //   setEdit(false);
+            toast.success("Customer added successfully");
+            //   setOperationSuccess(false); // Reset success state after handling success
+        }
+        // else if (update_success && operationSuccess) {
+        //   setIsOpen(false);
+        //   setEdit(false);
+        //   toast.success("Customer updated successfully");
+        //   setOperationSuccess(false); // Reset success state after handling success
+        // }
+
+        // Reset edit state if the popover is closed
+        // if (!isOpen) {
+        //   setEdit(false);
+        // }
+    }, [error, isSuccess, isOpen]);
     return (
         <div>
             <Button onClick={() => setIsOpen(true)}>Add new</Button>
